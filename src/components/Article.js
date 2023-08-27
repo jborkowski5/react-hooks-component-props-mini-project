07900559dfc3b7ca24ca40fccc46c9cbe77e3bc0
Joseph <borkowski.joseph@gmail.com>
2023-08-27 10:19:28 -0400

    complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-function Article({ title, date, preview }) {
+function Article({ title, date, preview, minutes }) {
   const formattedDate = date || "January 1, 1970";
+
+  // Calculate the number of emojis based on reading time
+  const coffeeCups = Math.ceil(minutes / 5);
+  const bentoBoxes = Math.ceil(minutes / 10);
+
+  // Generate emojis based on reading time
+  const coffeeEmoji = "☕️".repeat(coffeeCups);
+  const bentoEmoji = "🍱".repeat(bentoBoxes);
 
   return (
     <article>
       <h3>{title}</h3>
-      <small>{formattedDate}</small>
+      <small>{formattedDate} • {coffeeEmoji || bentoEmoji} {minutes} min read</small>
       <p>{preview}</p>
     </article>
   );
